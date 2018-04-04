9bada70b8a85825b871be1250df61d284db12aa0
Christopher Sean Connelly <connell7@data.cs.purdue.edu>
2018-04-04 13:29:34 -0400

    Added auth file, going through the process of passing the linter test tabs > spaces

--- src/log.cc
+++ src/log.cc
@@ -1,40 +1,33 @@
+// Copyright 2018 hello
 #include <string>
 #include "server.hh"
 
-void Log::write()
-{
-
+void Log::write() {
 }
 
-void Log::addReqTime(long length)
-{
-	if (length > max_request)
-	{
-		max_request = length;
-	}
-	if(min_request == 0 || length < min_request)
-	{
-		min_request = length;
-	}
+void Log::addReqTime(long length) {
+    if (length > max_request) {
+        max_request = length;
+    }
+    if (min_request == 0 || length < min_request) {
+        min_request = length;
+    }
 }
-std::string Log::generate_logs()
-{
-	std::string hello;
-	for(std::string s : request_list)
-	{
-		hello += s.c_str();
-		hello += '\n';
-	}
-	return hello;
+std::string Log::generate_logs()  {
+    std::string hello;
+    for (std::string s : request_list) {
+        hello += s.c_str();
+        hello += '\n';
+    }
+    return hello;
 }
-std::string Log::generate_stats()
-{
-	std::stringstream stat;
-	stat << "Christopher Sean Connelly\n";
-	stat << "Uptime (seconds): " << (time(NULL) - start_time) << "\n"; 
-	stat << "Requests: " << request_list.size() << "\n";
-	stat << "Max request time (ms): " << max_request << "\n";
-	stat << "Min request time (ms): " << min_request << "\n";
+std::string Log::generate_stats() {
+    std::stringstream stat;
+    stat << "Christopher Sean Connelly\n";
+    stat << "Uptime (seconds): " << (time(NULL) - start_time) << "\n";
+    stat << "Requests: " << request_list.size() << "\n";
+    stat << "Max request time (ms): " << max_request%100 << "\n";
+    stat << "Min request time (ms): " << min_request << "\n";
 
-	return stat.str();
+    return stat.str();
 }
